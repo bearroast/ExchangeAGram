6344d1c377cdd22d0bf5756a7f25859262f5d7db
Bjørn Rostad <hi@bjornrostad.no>
2015-04-08 20:04:42 -0700

    camera controller

---
+++ ExchangeAGram/FeedCell.swift
@@ -0,0 +1,16 @@
+//
+//  FeedCell.swift
+//  ExchangeAGram
+//
+//  Created by Bjørn Rostad on 3/29/15.
+//  Copyright (c) 2015 Bjørn Rostad. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+}
---
+++ ExchangeAGram/FeedViewController.swift
@@ -0,0 +1,77 @@
+//
+//  FeedViewController.swift
+//  ExchangeAGram
+//
+//  Created by Bjørn Rostad on 3/28/15.
+//  Copyright (c) 2015 Bjørn Rostad. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+class FeedViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func snapBarButtonItemTapped(sender: UIBarButtonItem) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) {
+            var cameraController = UIImagePickerController()
+            cameraController.delegate = self
+            cameraController.sourceType = UIImagePickerControllerSourceType.Camera
+            
+            
+            let mediaTypes:[AnyObject] = [kUTTypeImage]
+            cameraController.mediaTypes = mediaTypes
+            cameraController.allowsEditing = false
+            
+            self.presentViewController(cameraController, animated: true, completion: nil)
+        }
+        
+        else if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.PhotoLibrary) {
+            var photoLibraryController = UIImagePickerController()
+            photoLibraryController.delegate = self
+            photoLibraryController.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+            
+            let mediaTypes:[AnyObject] = [kUTTypeImage]
+            photoLibraryController.mediaTypes = mediaTypes
+            photoLibraryController.allowsEditing = false
+            
+            self.presentViewController(photoLibraryController, animated: true, completion: nil)
+        }
+        
+        else {
+            var alertController = UIAlertController(title: "Error", message: "Not good", preferredStyle: UIAlertControllerStyle.Alert)
+            alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alertController, animated: true, completion: nil)
+        }
+        
+    }
+    
+    
+    // UICollectionViewDataSource
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+
+}
